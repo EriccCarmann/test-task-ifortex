66aa25eeb06490d564ae0cd89c3234e0c326d8fd
Sasha Manyonok <83176347+EriccCarmann@users.noreply.github.com>
2024-12-10 12:14:44 +0300

    added project

---
+++ TestTask/Services/Implementations/AuthorService.cs
@@ -0,0 +1,18 @@
+﻿using TestTask.Models;
+using TestTask.Services.Interfaces;
+
+namespace TestTask.Services.Implementations
+{
+    public class AuthorService : IAuthorService
+    {
+        public Task<Author> GetAuthor()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Author>> GetAuthors()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ TestTask/Services/Implementations/BookService.cs
@@ -0,0 +1,18 @@
+﻿using TestTask.Models;
+using TestTask.Services.Interfaces;
+
+namespace TestTask.Services.Implementations
+{
+    public class BookService : IBookService
+    {
+        public Task<Book> GetBook()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Book>> GetBooks()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
